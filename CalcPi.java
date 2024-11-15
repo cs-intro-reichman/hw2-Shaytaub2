b41e31ad2f82ab87d95baf70caa5bdcf005b38a5
shaytaub2 <shaytaub2@gmail.com>
2024-11-15 12:13:48 +0200

    fix calcpi

--- CalcPi.java
+++ CalcPi.java
@@ -2,7 +2,7 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 		int n = Integer.parseInt(args[0]);
-		System.out.println("according to Java: " + Math.PI);
+		System.out.println("pi according to Java: " + Math.PI);
 		double pi = 1;
 		double x = 3;
 		double y = 5;
@@ -33,6 +33,6 @@ public class CalcPi {
 			}
 		}
 		pi = pi*4;
-		System.out.println("approximated: " + pi);
+		System.out.print("pi, approximated:	 " + pi);
 	}
 }
