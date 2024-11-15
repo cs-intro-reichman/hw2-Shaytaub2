75fdcfc6d3439077de89b113f107dbd32b0e155c
shaytaub2 <shaytaub2@gmail.com>
2024-11-15 11:46:18 +0200

    add all the files

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,38 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+		int n = Integer.parseInt(args[0]);
+		System.out.println(Math.PI);
+		double pi = 1;
+		double x = 3;
+		double y = 5;
+		if (n%2==0)
+		{
+			for (int i=0; i<=n; i++)
+			{
+				pi = pi - (1/x);
+				x +=4;
+			}
+			for(int j=0; j<n; j++)
+			{
+				pi = pi + (1/y);
+				y +=4;
+			}
+		}
+		else
+		{
+			for (int i=0; i<n; i++)
+			{
+				pi = pi - (1/x);
+				x +=4;
+			}
+			for(int j=0; j<=n; j++)
+			{
+				pi = pi + (1/y);
+				y +=4;
+			}
+		}
+		pi = pi*4;
+		System.out.println(pi);
 	}
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,42 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
+	public static void main(String args[]) 
+	{
+		int seed = Integer.parseInt(args[0]);
+		char mode = args[1].charAt(0);
+		int x;
+		int count;
+		int countB =0;
+		for (int i=0; i<seed; i++)
+		{
+			x = i+1;
+			count = 1;
+			String arr = "" + x;
+			while (x>1)
+			{
+				if (x%2==0)
+				{
+					x = x/2;
+					arr = arr + " " + x;
+					count = count + 1;
+				}
+				else
+				{
+					x = x*3+1;
+					arr = arr + " " + x;
+					count = count + 1;
+				}
+			}
+			arr = arr + " (" + count + ")";
+			if (x==1) {countB++;}
+			if (mode =='v')
+			{
+				System.out.println(arr);
+			}
+		}
+		if (countB == seed)
+			{
+				System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+			}
 	}
 }
