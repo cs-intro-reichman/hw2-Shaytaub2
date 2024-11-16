5bc2c8e25bc68917b3ab311c41fbdf24627506c0
shaytaub2 <shaytaub2@gmail.com>
2024-11-16 14:37:24 +0200

    fix calcpi

--- CalcPi.java
+++ CalcPi.java
@@ -3,23 +3,21 @@ public class CalcPi {
 	public static void main(String [] args) { 
 		int n = Integer.parseInt(args[0]);
 		System.out.println("pi according to Java: " + Math.PI);
-		double pi = 1.0;
-		double x = 3.0; //the odd places
-		double y = 5.0; //the even places
-		for (int i=0; i<=n; i++)
+		double pi = 0.0;
+		double x = 1.0;
+		for (int i=0; i<n; i++)
 		{
-			if (i%2!=0)
+			if (i%2==0)
 			{
-				pi = pi + (1.0/y);
-				y+=4;
+				pi +=(1.0/x);
 			}
 			else
 			{
-				pi = pi - (1.0/x);
-				x+=4;
+				pi -=(1.0/x);
 			}
+			x+=2;
 		}
-		pi = pi*4;
+		pi *=4;
 		System.out.print("pi, approximated:     " + pi);
 	}
 }
